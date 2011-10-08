7df33c8b683859d86ef2f272c0576542a5479e98
Hezi Cohen <hezi.co@gmail.com>
2011-10-08 13:04:57 +0200

    No need to import QuartzCore

--- HZActivityIndicator/HZActivityIndicatorView.m
+++ HZActivityIndicator/HZActivityIndicatorView.m
@@ -33,7 +33,6 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-#import <QuartzCore/QuartzCore.h>
 #import "HZActivityIndicatorView.h"
 
 @interface HZActivityIndicatorView ()
